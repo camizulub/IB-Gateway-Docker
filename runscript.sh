0f0a2cd8d16348d23859bd609a5f4f18c69aed33
Camilo Zuluaga <49444585+camizulub@users.noreply.github.com>
2021-03-17 13:28:17 -0500

    Update runscript.sh

--- runscript.sh
+++ runscript.sh
@@ -7,5 +7,5 @@ tail -f $(find $LOG_PATH -maxdepth 1 -type f -printf "%T@ %p\n" | sort -n | tail
 
 # Give enough time for a connection before trying to expose on 0.0.0.0:4003
 sleep 30
-echo "Forking :::4001 onto 0.0.0.0:4003\n"
-socat TCP-LISTEN:4003,fork TCP:127.0.0.1:4001
+echo "Forking :::4001 onto 0.0.0.0:7497\n"
+socat TCP-LISTEN:7497,fork TCP:127.0.0.1:7497
